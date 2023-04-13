0341d56f64bfed44d0627e6329ade6527b6e2479
Deepak <57728553+Deepak986786@users.noreply.github.com>
2023-04-13 14:03:39 +0530

    updated UI

--- src/App.js
+++ src/App.js
@@ -1,74 +1,91 @@
-import axios from 'axios';
-import React ,{useState,useEffect} from 'react';
-import Coins from './Coins';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Coins from "./Coins";
 
 const App = () => {
-    
-    const [data , setData]=useState([]);
-    const [search , setSearch]=useState([]);
- const uri="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState([]);
+  const uri =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
-  const getdata=()=>{
-    axios.get(uri)
-    .then(res => {
-      
-      setData(res.data);
-    })
-    .catch(err=>{
-      alert(err)
-    })
-   }
-   
-   useEffect(()=>{
-    setTimeout(()=>{getdata()},100)
+  const staticData = [];
 
-   },[])
-   
-   
+  const getdata = () => {
+    axios
+      .get(uri)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
- 
+  useEffect(() => {
+    setTimeout(() => {
+      getdata();
+    }, 100);
+  }, []);
 
+  const handleChangeofinputvalue = (e) => {
+    setSearch(e.target.value);
+  };
+  console.log(search.toString().toLowerCase());
 
-  const handleChangeofinputvalue=(e)=>{
-       setSearch(e.target.value);
-  }
-     console.log(search.toString().toLowerCase());
-
-  const filteredCoinonsearch=data.filter(coin=>
+  const filteredCoinonsearch = data.filter((coin) =>
     coin.name.toString().toLowerCase().includes(search.toString().toLowerCase())
   );
 
-  console.log(data)
+  console.log(data);
   return (
-          <> 
-             
-             <div className="bg-gradient-to-tr from-green-400 to-blue-700">
-          
-           <label htmlFor="Search">Search here</label>
-          <br></br><br />  
-                
-                    <form className="items-center justify-center bg-center ">
-                   <input type="text" placeholder="search" onChange={handleChangeofinputvalue} />
-                   </form> 
-                 { filteredCoinonsearch.map((curEle)=>{
-                    return(
-                        <div className="justify-center bg-red-600 border-4 shadow-2xl">
-                        <Coins key={curEle.id} 
-                               name={curEle.name}
-                               image={curEle.image}
-                               symbol={curEle.symbol}
-                               price={curEle.current_price}
-                               change={curEle.price_change_percentage_24h}
-                               marketcap={curEle.market_cap}
-                        />
-                       </div>
-               )
-          })
-         }
-        
-         </div>   
-        </>
-  )
-}
+    <>
+      <div className="bg-gradient-to-r from-green-200 to-indigo-600 grid grid-cols-1 sm:grid-cols-2 p-5 justify-center align-items-center">
+        <div className="logo grid sm:justify-center">
+          <h1 className="text-4xl font-extrabold gradient">CryptoMudra</h1>
+        </div>
+        <div className="search grid justify-center align-items-center p-1 rounded-sm">
+          <input
+            type="text"
+            className=""
+            placeholder="Search"
+            onChange={handleChangeofinputvalue}
+          />
+        </div>
+      </div>
+      <div>
+        <div className="table-responsive">
+          <table className="min-w-full">
+            <thead>
+              <tr className="m-2 bg-gray-400">
+                <th className="m-2 p-4">#</th>
+                <th className="m-2 p-4">Symbol</th>
+                <th className="m-2 p-4">Coin</th>
+                <th className="m-2 p-4">Price</th>
+                <th className="m-2 p-4">24h change</th>
+                <th className="m-2 p-4">marketcap</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredCoinonsearch.map((curEle, index) => {
+                return (
+                  <Coins
+                    rank={index}
+                    id={curEle.id}
+                    name={curEle.name}
+                    image={curEle.image}
+                    symbol={curEle.symbol}
+                    price={curEle.current_price}
+                    change={curEle.price_change_percentage_24h}
+                    marketcap={curEle.market_cap}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/Coins.js
+++ src/Coins.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Coins = ({name ,symbol,image,price,change,marketcap}) => {
+const Coins = ({rank,id,name ,symbol,image,price,change,marketcap}) => {
     return (
 
-         <div className="grid items-center justify-center grid-cols-7 bg-green-400 bg-trcolansparent mr-auto- inlline-flex coinContainer">
-             <div className="inline coinimg"><img src={image} className="w-8 rounded-2xl" alt="imageurl"/></div>
-             <div className="inline coinname" >{name}</div>
-             <div className="inline coinsymbol">{symbol.toString().toUpperCase()}</div>
-             <div className="inline border-b-2 coinprice">{price}</div>
-             <div className="inline coinchange">{change}</div>
-             <div className="inline coinvol"></div>
-             <div className="inline coinmcap">{marketcap}</div>
-         </div>
-        
+           <tr key={id} className="bg-gray-200 max-w-sm border-2 border-gray-500">
+                 <td className="m-2 p-4 text-center">{++rank}</td>
+                <td className="m-2 text-center flex justify-center"><img src={image} className="rounded-2xl w-10 align-self-center" alt={symbol}/></td>
+                <td className="m-2 p-4 text-center">{symbol.toString().toUpperCase()}</td>
+                <td className="m-2 p-4 text-center ">{price}</td>
+                <td className="m-2 p-4 text-center ">{change.toString().toUpperCase()}</td>
+                <td className="m-2 p-4 text-center ">{marketcap.toString().toUpperCase()}</td>
+           </tr> 
     )
 }
  
