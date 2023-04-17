6be8b23a0c2e9561b100f3d7e1d82d0806b730c1
Deepak <57728553+Deepak986786@users.noreply.github.com>
2023-04-17 12:53:34 +0530

    fix alignment

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ const App = () => {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState([]);
   const uri =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false";
 
-  const staticData = [];
 
   const getdata = () => {
     axios
